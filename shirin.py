b97531a7dccb7e7283d7d4f8d04a457aaa080efb
Najnin Sultana Shirin <najninshirin26@gmail.com>
2025-02-04 11:22:42 +0600

    Create shirin.py

---
+++ shirin.py
@@ -0,0 +1,13 @@
+Hi , there!
+
+I am Najnin Sultana Shirin.
+
+I am currently studing in  Computer Science at the Jagannath University, Bangladesh.
+
+I’m currently working on Computer Vision , Machine Learning
+
+I’m currently learning Machine Learning, Deep Learning and Computer Vision!
+
+I’m looking to collaborate on Computer Vision , Machine Learning
+
+How to reach me najninshirin26@gmail.com .
